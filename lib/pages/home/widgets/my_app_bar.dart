3ff552e7d561e61982f4bb9ffbefdefc8030086d
Liliadetassia <lylyatavares@2gmail.com>
2022-02-09 18:16:15 -0300

    icons

--- lib/pages/home/widgets/my_app_bar.dart
+++ lib/pages/home/widgets/my_app_bar.dart
@@ -9,25 +9,31 @@ class MyAppBar extends StatelessWidget {
           height: MediaQuery.of(context).padding.top,
         ),
         Container(
-          color: Colors.red,
-          height: 200,
-          child: Row(
-            children: <Widget>[
-              Image.network(
-                'https://i.pinimg.com/originals/90/d8/f1/90d8f1af934daaa634246c79f5a7ea4f.png',
-                height: 35,
-                color: Colors.white,
+          color: Colors.purple[800],
+          height: 120,
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Image.network(
+                    'https://i.pinimg.com/originals/90/d8/f1/90d8f1af934daaa634246c79f5a7ea4f.png',
+                    height: 35,
+                    color: Colors.white,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    'Lília de Tássia',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
               ),
-              SizedBox(
-                width: 10,
-              ),
-              Text(
-                'Lília de Tássia',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18,
-                ),
-              ),
+              Icon(Icons.expand_more)
             ],
           ),
         ),
