91886be9f7c7387d74ed399a086d00217e3cf0ca
Liliadetassia <lylyatavares@2gmail.com>
2022-02-09 18:36:05 -0300

    adicionando_GestureDetector

--- lib/pages/home/widgets/my_app_bar.dart
+++ lib/pages/home/widgets/my_app_bar.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 
 class MyAppBar extends StatelessWidget {
   final bool showMenu;
+  final VoidCallback onTap;
 
-  const MyAppBar({Key? key, this.showMenu}) : super(key: key);
+  const MyAppBar({Key? key, this.showMenu, this.onTap}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -11,34 +12,37 @@ class MyAppBar extends StatelessWidget {
         SizedBox(
           height: MediaQuery.of(context).padding.top,
         ),
-        Container(
-          color: Colors.purple[800],
-          height: 120,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Image.network(
-                    'https://i.pinimg.com/originals/90/d8/f1/90d8f1af934daaa634246c79f5a7ea4f.png',
-                    height: 35,
-                    color: Colors.white,
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Text(
-                    'Lília de Tássia',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18,
+        GestureDetector(
+          onTap: ,
+          child: Container(
+            color: Colors.purple[800],
+            height: 120,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Image.network(
+                      'https://i.pinimg.com/originals/90/d8/f1/90d8f1af934daaa634246c79f5a7ea4f.png',
+                      height: 35,
+                      color: Colors.white,
                     ),
-                  ),
-                ],
-              ),
-              Icon(!showMenu ? Icons.expand_more : Icons.expand_less)
-            ],
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      'Lília de Tássia',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ],
+                ),
+                Icon(!showMenu ? Icons.expand_more : Icons.expand_less)
+              ],
+            ),
           ),
         ),
       ],
