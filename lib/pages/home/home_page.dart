a0eb81b2885d84a267851461851d3a6720c2703e
Liliadetassia <lylyatavares@2gmail.com>
2022-02-10 21:58:16 -0300

    Positioned

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -11,17 +11,23 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   late bool _showMenu;
   late int _currentIndex;
+  late double _yPosition;
 
   @override
   void initState() {
     super.initState();
     _showMenu = false;
     _currentIndex = 0;
+    _yPosition = 0;
   }
 
   @override
   Widget build(BuildContext context) {
     double _screenHeigth = MediaQuery.of(context).size.height;
+    if (_yPosition == null) {
+      _yPosition = _screenHeigth * .24;
+    }
+
     var child2 = null;
     return Scaffold(
       backgroundColor: Colors.purple[800],
@@ -33,14 +39,52 @@ class _HomePageState extends State<HomePage> {
             onTap: () {
               setState(() {
                 _showMenu = !_showMenu;
+                _yPosition =
+                    _showMenu ? _screenHeigth * .75 : _screenHeigth * .24;
               });
             },
           ),
           PageViewApp(
-            top: !_showMenu ? _screenHeigth * .24 : _screenHeigth * .75,
+            top: _yPosition,
             onChanged: (index) {
               setState(() {
                 _currentIndex = index;
+              });
+            },
+            onPanUpdate: (details) {
+              double positionBottomLimit = _screenHeigth * .75;
+              double positionTopLimit = _screenHeigth * .24;
+              double midlePosition = positionBottomLimit - positionTopLimit;
+              midlePosition = midlePosition / 2;
+              setState(() {
+                _yPosition += details.delta.dy;
+
+                _yPosition = _yPosition < positionTopLimit
+                    ? positionTopLimit
+                    : _yPosition;
+
+                _yPosition = _yPosition > positionBottomLimit
+                    ? positionBottomLimit
+                    : _yPosition;
+
+                if (_yPosition != positionBottomLimit && details.delta.dy > 0) {
+                  _yPosition =
+                      _yPosition > positionTopLimit + midlePosition - 50
+                          ? positionBottomLimit
+                          : _yPosition;
+                }
+
+                if (_yPosition != positionTopLimit && details.delta.dy < 0) {
+                  _yPosition = _yPosition < positionBottomLimit - midlePosition
+                      ? positionTopLimit
+                      : _yPosition;
+                }
+
+                if (_yPosition == positionBottomLimit) {
+                  _showMenu = true;
+                } else if (_yPosition == positionBottomLimit) {
+                  _showMenu = false;
+                }
               });
             },
           ),
